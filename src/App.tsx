15da81cca3735b1093f70487603247495efa4298
andreikostsen <pks@tut.by>
2023-02-16 23:19:46 +0300

    minsk coordinates added

--- src/App.tsx
+++ src/App.tsx
@@ -10,13 +10,13 @@ function App() {
 
     <div>
 
-    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}
+    <MapContainer center={[53.884, 27.523]} zoom={13} scrollWheelZoom={false}
                   style={{ width: "100%", height: "100vh" }}>
         <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[51.505, -0.09]}>
+        <Marker position={[53.884, 27.523]}>
             <Popup>
                 A pretty CSS3 popup. <br /> Easily customizable.
             </Popup>
