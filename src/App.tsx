b8cabd56152e7d61a2db51030a75661c40586552
andreikostsen <pks@tut.by>
2023-02-17 23:42:51 +0300

    markers array started

--- src/App.tsx
+++ src/App.tsx
@@ -4,28 +4,33 @@ import React from 'react';
 
 import {MapContainer, Marker, Popup, TileLayer, useMap} from 'react-leaflet'
 
+const markers = [
+    {id: 1, title: "point1", latitude: 53.884, longitude: 27.600, info: "Some info about point 1"},
+    {id: 2, title: "point2", latitude: 53.9024716, longitude: 27.5618225, info: "Some info about point 2"},
+]
+
+
+
 function App() {
   return (
 
 
     <div>
 
-    <MapContainer center={[53.884, 27.523]} zoom={13} scrollWheelZoom={false}
+    <MapContainer center={[53.884, 27.523]} zoom={13} scrollWheelZoom={true}
                   style={{ width: "100%", height: "100vh" }}>
         <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[53.884, 27.523]}>
-            <Popup>
-                A pretty CSS3 popup. <br /> Easily customizable.
-            </Popup>
-        </Marker>
-        <Marker position={[53.900, 27.530]}>
-            <Popup>
-                A pretty CS popup. <br /> Easily customizable.
-            </Popup>
-        </Marker>
+
+        {markers.map(m=><Marker position={[m.latitude, m.longitude]}>
+                <Popup>
+                    <h3>{m.title}</h3>
+                    {m.info}
+                </Popup>
+            </Marker>
+        )}
     </MapContainer>
 
 
