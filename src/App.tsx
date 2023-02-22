585bea2d290eb50984b48de61616f7ce72a0cb5a
andreikostsen <pks@tut.by>
2023-02-22 17:46:18 +0300

    state and json started

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,8 @@ import React from 'react';
 // import from './App.css';
 
 import {MapContainer, Marker, Popup, TileLayer, useMap} from 'react-leaflet'
+import {markers} from "./state/state";
 
-const markers = [
-    {id: 1, title: "point1", latitude: 53.884, longitude: 27.600, info: "Some info about point 1"},
-    {id: 2, title: "point2", latitude: 53.9024716, longitude: 27.5618225, info: "Some info about point 2"},
-]
 
 
 
---
+++ src/state/state.js
@@ -0,0 +1,5 @@
+
+export const markers = [
+    {id: 1, title: "point1", latitude: 53.884, longitude: 27.600, info: "Some info about point 1"},
+    {id: 2, title: "point2", latitude: 53.9024716, longitude: 27.5618225, info: "Some info about point 2"},
+]
