4d87511402292f165115b59b69a26c8da34da54c
andreikostsen <pks@tut.by>
2023-02-24 16:25:21 +0300

    filter started

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,30 @@ import React from 'react';
 // import from './App.css';
 
 import {MapContainer, Marker, Popup, TileLayer, useMap} from 'react-leaflet'
-import {markers} from "./state/state";
+import {markers, waste} from "./state/state";
 
 
 
 
 function App() {
+
+    const filter = "all "
+
+
+    const filteredMarkers = markers.filter(m=>);
+
+
+
+
   return (
 
 
     <div>
-
+        <button style={{background: "green"}} >Пластик</button>
+        <button>Бумага</button>
+        <button>Стекло</button>
     <MapContainer center={[53.884, 27.523]} zoom={13} scrollWheelZoom={true}
-                  style={{ width: "100%", height: "100vh" }}>
+                  style={{ width: "100%", height: "50vh" }}>
         <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -24,7 +35,9 @@ function App() {
         {markers.map(m=><Marker position={[m.latitude, m.longitude]}>
                 <Popup>
                     <h3>{m.title}</h3>
-                    {m.info}
+                    {m.address}<br/>
+                    {m.info}<br/>
+                    Перерабатываем: {m.wasteType}
                 </Popup>
             </Marker>
         )}
--- src/state/state.js
+++ src/state/state.js
@@ -1,5 +1,10 @@
 
+export const waste = ["пластик", "бумага", "стекло", "крупногабаритные отходы", "опасные отходы", "металл"]
+
+
+// ["plastic", "paper", "glass", "oversized", "dangerous", "metal"]
+
 export const markers = [
-    {id: 1, title: "point1", latitude: 53.884, longitude: 27.600, info: "Some info about point 1"},
-    {id: 2, title: "point2", latitude: 53.9024716, longitude: 27.5618225, info: "Some info about point 2"},
+    {id: 1, title: "Пункт 1", address: "г.Минск, ул.Сурганова, 23", latitude: 53.884, longitude: 27.600, info: "Some info about point 1", wasteType: [waste[0], waste[1]] },
+    {id: 2, title: "Пункт 2", address: "г.Минск, ул.Кальварийская, 7", latitude: 53.9024716, longitude: 27.5618225, info: "Some info about point 2", wasteType: [waste[1], waste[2]]},
 ]
