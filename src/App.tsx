a40f7336aceb3e33f3ca196a3b0218d4ee3025d7
andreikostsen <pks@tut.by>
2023-09-01 16:36:07 +0300

    fetch markers from My JSON Server

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import React, {useState} from 'react';
-
-import s from './App.module.scss';
-
-import {MapContainer, Marker, Popup, TileLayer, useMap} from 'react-leaflet'
-import {ButtonObjType, buttons, markers, waste} from "./state/state";
+import {useState} from 'react';
+import L from 'leaflet';
+import {MapContainer, Marker, Popup, TileLayer} from 'react-leaflet'
+import MarkerClusterGroup from "react-leaflet-cluster";
+import {ButtonObjType, buttons, markers, MarkerType, waste} from "./state/state";
 import {Button} from "./Button";
+import s from './App.module.scss';
+import pointIcon from './point_icon.svg';
+import axios, {AxiosError} from 'axios';
 
 
 // export type filterType = "all" | "plastic" | "paper" | "glass"
@@ -28,6 +30,35 @@ function App() {
 
     }
 
+    const customIcon = new L.Icon({
+        iconUrl: pointIcon,
+        iconSize: new L.Point(27, 32)
+    });
+
+
+
+// API
+    const instance = axios.create({baseURL: 'https://my-json-server.typicode.com/andreikostsen/testsearch/'})
+
+    const api = {
+        getMarkers() {
+            return instance.get<MarkerType[]>('markersState')
+        },
+
+    }
+
+        api.getMarkers()
+            .then((res) => {
+                console.log(typeof (res.data[1].wasteTypes))
+
+            })
+            .catch((e: AxiosError) => {
+                console.log(e)
+            })
+
+
+
+
     let filteredMarkers = markers;
 
     filteredMarkers.map(m => m.display = false)
@@ -38,13 +69,16 @@ function App() {
         }
     }
 
+    const token = 'xZpKoSPd2lxvjHa2OY9UT0kBT6StaY0c7pnbhNF1RPCPKAexPRuo2P8x8KKICtO3';
+
 
     return (
 
         <section className={s.map}>
             <h2>Куда сдать?</h2>
             <div className={s.buttonsWrapper}>
-                {buttons.map(b => <Button
+                {buttons.map((b, i) => <Button
+                    key = {i}
                     title={b.wasteTitle}
                     changeButtonStatus={changeButtonStatus}
                     isActive={b.isActive}
@@ -52,24 +86,51 @@ function App() {
             </div>
             <div>
 
-                <MapContainer center={[53.884, 27.523]} zoom={11.5} scrollWheelZoom={true}
+                <MapContainer center={[53.9024716, 27.5618225]} zoom={11.5} scrollWheelZoom={true}
                               className={s.mapContainer}>
                     <TileLayer
-                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        attribution='<a href=\"https://www.jawg.io\" target=\"_blank\">&copy; Jawg</a> - <a href=\"https://www.openstreetmap.org\" target=\"_blank\">&copy; OpenStreetMap</a>&nbsp;contributors'
+                        url={`https://tile.jawg.io/jawg-streets/{z}/{x}/{y}{r}.png?access-token=${token}&lang=ru`}
                     />
-
-                    {
-                        filteredMarkers.map(m =>
-                            m.display ? <Marker position={[m.latitude, m.longitude]}>
-                                <Popup>
-                                    <h3>{m.title}</h3>
-                                    {m.address}<br/>
-                                    {m.info}<br/>
-                                    Перерабатываем: {m.wasteTypes}
+                    <MarkerClusterGroup chunkedLoading>
+                    {filteredMarkers.map((m, i) =>
+                            m.display ? <Marker key={i} position={[m.latitude, m.longitude]} icon={customIcon}>
+                                {/*<Popup>*/}
+                                {/*    <h3>{m.title}</h3>*/}
+                                {/*    {m.address}<br/>*/}
+                                {/*    {m.info}<br/>*/}
+                                {/*    Перерабатываем: {m.wasteTypes}*/}
+                                {/*</Popup>*/}
+                                <Popup className={s.popup} keepInView={false} maxWidth={370}>
+                                    <div className={s.popupHeader}>{m.title}</div>
+                                    <div className={s.popupAddressWrapper}>
+                                        <ul className={s.address}>
+                                            <li className={s.locationPoint}>
+                                                <div className={s.popupAddressContent}>{m.address}</div>
+                                            </li>
+                                            <li className={s.phone}>
+                                                <div className={s.popupAddressContent}>{m.phone}</div>
+                                            </li>
+                                            <li className={s.schedule}>
+                                                <div className={s.popupAddressContent}>{m.schedule}</div>
+                                            </li>
+                                            <li className={s.website}>
+                                                <div className={s.popupAddressContent}><a href={m.website}>{m.website}</a></div>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                    <div className={s.popupFooter}>
+                                        Перерабатываем:
+                                        <ul className={s.wasteTypes}>
+                                            {m.wasteTypes.map((item, i) =>
+                                                <li key={i}>{item}</li>
+                                            )}
+                                        </ul>
+                                    </div>
                                 </Popup>
                             </Marker> : ""
                         )}
+                    </MarkerClusterGroup>
                 </MapContainer>
             </div>
 
